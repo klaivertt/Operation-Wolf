a13f60054d321364977af8a82a36adabef81a8e3
Klaivert <69586612+klaivertt@users.noreply.github.com>
2024-12-11 13:48:04 +0100

    map

--- Prog/Game/Map.h
+++ Prog/Game/Map.h
@@ -5,50 +5,6 @@
 #include "Common.h"
 #include "Sprite.h"
 
-typedef enum EnemyState
-{
-	WALK,
-	SHOOT,
-	DEAD,
-
-}EnemyState;
-
-typedef enum EnemyType
-{
-	SOLDIER,
-}EnemyType;
-
-
-typedef enum Drop
-{
-	HEALTH,
-	AMMO,
-	NONE
-}Drop;
-
-typedef struct Enemy
-{
-	EnemyType type;
-	EnemyState state;
-	Drop drop;
-
-	int life;
-	int damage;
-	float waitTime;
-	sfSprite* sprite;
-
-}Enemy;
-
-typedef struct EnemyData
-{
-	sfIntRect animSoldier[9];
-	int nbSoldierSprite;
-
-	sfTexture* spriteSheet;
-
-}EnemyData;
-
-
 void LoadMap(void);
 void UpdateMap(float _dt);
 void DrawMap(sfRenderWindow* _renderWindow);
