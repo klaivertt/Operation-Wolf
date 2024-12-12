457e81deccc347bca159082d95d0447ae67b6324
kUwUllion <kyllian.nb@outlook.fr>
2024-12-12 09:36:20 +0100

    ajt hud balles dans le chargeur

--- Prog/Game/Bullet.h
+++ Prog/Game/Bullet.h
@@ -2,11 +2,19 @@
 #ifndef BULLTE_H
 #define BULLET_H
 
+#define BULLET_NUMBER_MAX 15
+
 #include "Common.h"
 #include "Sprite.h"
 
+typedef struct Bullet
+{
+	sfSprite* sprite[BULLET_NUMBER_MAX];
+	sfTexture* texture;
+}Bullet;
+
 void LoadBullet();
-void UpdateBullet();
+void UpdateBullet(float _dt);
 void DrawBullet(sfRenderWindow* _renderWindow);
 void CleanupBullet();
 
