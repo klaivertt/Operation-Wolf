aef0cb1450f7eb0e8daf5c5e48437e2bbc527cc2
Klaivert <69586612+klaivertt@users.noreply.github.com>
2024-12-13 09:42:26 +0100

    Update Bullet.h

--- Prog/Game/Bullet.h
+++ Prog/Game/Bullet.h
@@ -12,9 +12,21 @@ typedef struct Bullet
 	sfTexture* texture;
 }Bullet;
 
-void LoadBullet();
+//* @brief The LoadBullet function does not return any parameters, it just clamps the load.
+//*
+//* Loads the hud from the balls and transmits it to hud
+void LoadBullet(void);
+
+//* @brief Draws all active bullets on the given render window.
+//*
+//* This function iterates through all bullets and renders each one
+//* on the provided window.Only bullets that are currently active.
+//*
+//* @param _renderWindow Pointer to the CSFML render window where the bullets will be drawn.
 void DrawBullet(sfRenderWindow* _renderWindow);
-void CleanupBullet();
+
+//* @brief the CleanupBullet function is used to reload pointers and bullet values for the hud.
+void CleanupBullet(void);
 
 #endif // !BULLTE_H
 
