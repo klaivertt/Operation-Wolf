c46c3ef09b1ef69da616e69cefb7876064b5e882
Klaivert <69586612+klaivertt@users.noreply.github.com>
2024-12-20 11:58:30 +0100

    h

--- Prog/Game/GameSound.h
+++ Prog/Game/GameSound.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef GAMESOUND_H
 #define GAMESOUND_H
 
@@ -8,8 +7,18 @@
 #include "PlayerSound.h"
 #include "HostageSound.h"
 
+
+//* @brief Loads the sound resources for the game.
+//*
+//* This function initializes and prepares the sound files (e.g., background music,
+//* sound effects) to be used in the game, loading them into memory for playback. 
 void LoadGameSounds(void);
 
+
+//* @brief Frees the memory and resources associated with game sounds.
+//*
+//* This function ensures proper cleanup by deallocating any sound resources
+//* (e.g., sound buffers, sound instances) used in the game to prevent memory leaks.
 void CleanupGameSound(void);
 
 #endif
--- Prog/Game/PlayerSound.h
+++ Prog/Game/PlayerSound.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef PLAYERSOUNDS_H
 #define PLAYERSOUNDS_H
 
@@ -16,10 +15,33 @@ typedef struct PlayerSound
 
 }PlayerSound;
 
+
+//* @brief Loads the sound resources for the player.
+//*
+//* This function initializes and prepares the player-related sound files (e.g.,
+//* player shooting, reloading) to be used in the game, loading them into memory for playback.
 void LoadPlayerSounds(void);
+
+
+//* @brief Frees the memory and resources associated with player sounds.
+//*
+//* This function ensures proper cleanup by deallocating any sound resources
+//* (e.g., sound buffers, sound instances) related to the player to prevent memory leaks.
 void CleanupPlayerSound(void);
 
+//* @brief Plays the sound effect for the player shooting.
+//*
+//* This function triggers the sound associated with the player firing their weapon.
 void PlaySound_PlayerShoot(void);
+
+//* @brief Plays the sound effect for the player attempting to shoot without ammunition.
+//*
+//* This function triggers the sound associated with the player trying to shoot when no bullets are available.
 void PlaySound_PlayerShootWithoutBullet(void);
+
+//* @brief Plays the sound effect for the player reloading.
+//*
+//* This function triggers the sound associated with the player reloading their weapon.
 void PlaySound_PlayerReload(void);
+
 #endif
